3795fcbb01251f8b3e94a0a957115f39c012e066
Sean Treadway <treadway@gmail.com>
2024-10-15 12:30:42 +0200

    Checkpoint

---
+++ modules/nvim.nix
@@ -0,0 +1,30 @@
+{ config, lib, system, pkgs, ... }:
+
+{
+  programs.nixvim = {
+    enable = true;
+    enableMan = false;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    globals = {
+      mapleader = ",";
+      maplocalleader = ",";
+    };
+
+    opts = {
+      number = true;
+
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+
+      autoindent = true;
+      smartindent = true;
+      smarttab = true;
+
+      cursorline = true;
+    };
+  };
+}
