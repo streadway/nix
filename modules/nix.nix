913721e4af09fb9d4c121cd13a82a73045649f09
Sean Treadway <srt@veo.co>
2025-09-19 09:34:54 +0200

    Append substituters instead of replace

--- modules/nix.nix
+++ modules/nix.nix
@@ -7,13 +7,11 @@
     settings = {
       experimental-features = "nix-command flakes";
 
-      substituters = [
-        "https://cache.nixos.org/"
+      extra-substituters = [
         "https://nix-community.cachix.org"
       ];
 
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
 
