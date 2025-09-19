81559f2de3d7c634da09decd15a5c370bd07626b
Sean Treadway <srt@veo.co>
2025-09-19 08:40:13 +0200

    Add sean's pubkey for ws-srt

--- hosts/ws-srt.dev.core.veo.co.nix
+++ hosts/ws-srt.dev.core.veo.co.nix
@@ -12,6 +12,9 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
     shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoDCPzWaZ2g6eVgPUfVHWnpz67VO7GsKL9gxFuqLYJL srt@veo.co"
+    ];
   };
 
   programs.fish.enable = true;
--- modules/nix.nix
+++ modules/nix.nix
@@ -21,6 +21,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    git
     vim
   ];
 }
